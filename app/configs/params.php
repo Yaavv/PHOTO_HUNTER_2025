7d9fb46ac2e2523b367264d3dc1847b1801cebdc
Yaav <maximestruvay@gmail.com>
2025-04-17 15:36:56 +0200

    Structure MVC de base

---
+++ app/configs/params.php
@@ -0,0 +1,11 @@
+<?php
+// Initialisation des zones dynamiques
+$content = "hop";
+
+
+
+// Paramètres de connexion
+define('DB_HOST', 'localhost');
+define('DB_NAME', 'photo_hunter');
+define('DB_USER', 'root');
+define('DB_PASSWORD', 'root');
---
+++ core/init.php
@@ -0,0 +1,4 @@
+<?php
+
+require_once '../app/configs/params.php';
+require_once '../core/connexion.php';
