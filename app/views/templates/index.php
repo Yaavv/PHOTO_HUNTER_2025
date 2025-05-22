f985d902d596c7dae18416d0fbbb5618c2b072d4
Yaav <maximestruvay@gmail.com>
2025-05-22 13:16:37 +0200

    Initial commit - reprise du projet

---
+++ app/views/templates/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <?php include '../app/views/templates/partials/_head.php'; ?>
+</head>
+
+<body class="bg-gray-300">
+    <?php include '../app/views/templates/partials/_nav.php'; ?>
+
+    <?php include '../app/views/templates/partials/_main.php'; ?>
+
+    <?php include '../app/views/templates/partials/_footer.php'; ?>
+</body>
+
+</html>
---
+++ app/views/templates/partials/_aside.php
@@ -0,0 +1,6 @@
+<aside class="p-4 md:w-1/3">
+    <div class="h-full p-6">
+        <?php include '../app/views/templates/partials/_search.php'; ?>
+        <?php include '../app/views/categories/_index.php'; ?>
+    </div>
+</aside>
